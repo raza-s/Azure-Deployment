da4de8748000f0e22111806b867c1e549f426a07
raza-s <104041595@student.swin.edu.au>
2026-08-21 16:01:30 +1000

    Completed until add a view
    https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-view?view=aspnetcore-10.0&tabs=visual-studio
    
    Followed steps until add a view. Next stpes is add a model

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index() //Calls the controllers View method and generates a HTMLresponse.
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    //public string Welcome(string name)
+    //{
+    //    return "Hi " + name;
+    //}
+    //public string Welcome(string name, int numTimes = 1)
+    //{
+    //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    //}
+    //public string Welcome(string name, int ID = 1) //Post verfication can be seen below. HTMLEncoder prevents JS attacks.
+    //{
+    //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    //}
+    //Usage: https://localhost:7080/HelloWorld/Welcome/3?name=Raza
+
+    public IActionResult Welcome(string name, int numTimes = 1) //
+    {
+        ViewData["Message"] = "Hello " + name; //The ViewData dictionary object contains data that will be passed to the view.
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7c21c365fbf8bce34457fd2ca2319898f34911")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
