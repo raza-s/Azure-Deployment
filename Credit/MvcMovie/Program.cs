60a406909cc4c730879ec36e234c702861065a5f
raza-s <104041595@student.swin.edu.au>
2026-08-27 00:41:26 +1000

    Credit Task Complete
    We can add and remove movies.

---
+++ Credit/MvcMovie/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.");
+
+builder.Services.AddDbContext<MvcMovieContext>(options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
