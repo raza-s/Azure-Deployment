3f22d4c37777a4ebc0ef96aa501586a86c3c1c55
raza-s <104041595@student.swin.edu.au>
2026-08-26 22:41:46 +1000

    Structure changes

---
+++ Pass/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
